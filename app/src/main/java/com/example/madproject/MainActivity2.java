e1632100c6126363632ce23fb3e32daa48e45385
Dulan <dulanramanayaka12@gmail.com>
2020-08-16 17:27:21 +0530

    Task manage

---
+++ app/src/main/java/com/example/madproject/MainActivity2.java
@@ -0,0 +1,32 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btn = findViewById(R.id.button2);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
