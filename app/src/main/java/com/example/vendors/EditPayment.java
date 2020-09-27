c33a13b0dbcea76445e8c299519ede385c6ce1d4
deshani0000 <it19051826@my.sliit.lk>
2020-09-27 13:00:13 +0530

    y commit

--- app/src/main/java/com/example/vendors/EditPayment.java
+++ app/src/main/java/com/example/vendors/EditPayment.java
@@ -24,12 +24,7 @@ public class EditPayment extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu2,menu);
-        return true;
 
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
