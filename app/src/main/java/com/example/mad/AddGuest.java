301b8858e027307e69acc336576a84d07af28b51
dulshani110 <dulshanisd@gmail.com>
2020-09-27 13:08:08 +0530

    second commit

--- app/src/main/java/com/example/mad/AddGuest.java
+++ app/src/main/java/com/example/mad/AddGuest.java
@@ -2,13 +2,13 @@ package com.example.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
 public class AddGuest extends AppCompatActivity {
 
--- app/src/main/java/com/example/mad/MainActivity.java
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -47,10 +47,6 @@ public class MainActivity extends AppCompatActivity {
         guestAdapter gAdapter=new guestAdapter(context,R.layout.single_guest,Guest);
         todoguest.setAdapter(gAdapter);
 
-        //get the count of guests
-        int countguest=dbHandler.countGuest();
-
-        count.setText("You have "+countguest+" guests");
 
 
 
